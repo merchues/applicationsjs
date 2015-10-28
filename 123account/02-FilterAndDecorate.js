2d7cc6c79124bae18142950d0650ece0c92af49f
Alfonso Presa <apresaru@servexternos.isban.es>
2015-10-28 15:30:41 +0100

    Add custom event

--- 123account/02-FilterAndDecorate.js
+++ 123account/02-FilterAndDecorate.js
@@ -1,4 +1,4 @@
-dsEventBroker.when('*').then(function () {
+dsEventBroker.when('*').polish(function () {
     return {
         title: document.title
     }
@@ -6,4 +6,12 @@ dsEventBroker.when('*').then(function () {
 
 dsEventBroker.when('click').then(sendEvent);
 dsEventBroker.when('change').then(sendEvent);
+
+dsEventBroker.when('click','form>div:nth-child(5)>button:nth-child(2),form>div:nth-child(5)>button:nth-child(2) *').polish(function() {
+                return {
+                    customData: {
+                        'Account Name':$('label>p').text()
+                    }
+                };
+            }).then(sendEvent);
 
